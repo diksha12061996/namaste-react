48ddbf9bc409ab20f36d27e08e95cf42c6f269bc
Deeksha Rajput <diksharajput1206@gmail.com>
2023-05-31 07:33:36 +0530

    episode-01

---
+++ app.js
@@ -0,0 +1,5 @@
+const heading = React.createElement("div", { id: "Parent" }, [React.createElement("div", { id: "child" },
+    React.createElement("h1", { id: "h1" }, "Hello world")),
+React.createElement("h2", { id: "h2" }, "Hello world2")]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
