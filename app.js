20c2419e0c44122199be0fb16fa4b33c46280d3b
Deeksha Rajput <diksharajput1206@gmail.com>
2023-06-06 07:04:10 +0530

    episode-02

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
 const heading = React.createElement("div", { id: "Parent" }, [React.createElement("div", { id: "child" },
     React.createElement("h1", { id: "h1" }, "Hello world")),
 React.createElement("h2", { id: "h2" }, "Hello world2")]);
