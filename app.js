be3fe1ad9e5b8e7c9dfac1e325eaa7259e77b6aa
Deeksha Rajput <diksharajput1206@gmail.com>
2023-06-07 22:28:15 +0530

    episode-03

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const heading = React.createElement("div", { id: "Parent" }, [React.createElement("div", { id: "child" },
-    React.createElement("h1", { id: "h1" }, "Hello world")),
-React.createElement("h2", { id: "h2" }, "Hello world2")]);
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+// const heading = React.createElement("div", { id: "Parent" }, [React.createElement("div", { id: "child" },
+//     React.createElement("h1", { id: "h1" }, "Hello world")),
+// React.createElement("h2", { id: "h2" }, "Hello world2")]);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+//React element
+//const heading= React.createElement("h1",{id:"heading"},"Namaste 🙏")
+const JsxHeading = () => (
+    <h1 id='heading'
+        className='heading' tabIndex='1'>
+        React from JSX
+    </h1>)
+
+const HeadingComponent = () => (
+    // <React.Fragment>
+    <>
+        <div id='container'>
+            <JsxHeading />
+            <h1 className='heading'>This is React Functional component</h1>
+        </div>
+        <div id='container2'>This is React Functional component1</div>
+    </>
+    // </React.Fragment>
+);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<HeadingComponent />)
