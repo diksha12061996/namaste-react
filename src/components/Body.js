4a53218252856211a855723892f4de7b689c75df
Deeksha Rajput <diksharajput1206@gmail.com>
2023-06-16 06:23:24 +0530

    episode-06

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,73 +1,50 @@
 import RestaurantCard from "./RestaurantCard";
-import { useState } from "react";
-import resList from "../utils/mockData";
+import { useState, useEffect } from "react";
+import Shimmer from "./shimmer";
 const Body = () => {
-     let [listOfRestaurant,setListOfRestaurant] = useState(resList
-        //[
-    //     {
-    //         "type": "restaurant",
-    //         "data": {
-    //             "id": "65797",
-    //             "name": "Leon's - Burgers & Wings (Leon Grill)",
-    //             "deliveryTime": 20,
-    //             "cloudinaryImageId": "xqwpuhgnsaf18te7zvtv",
-    //             "cuisines": ["American", "Snacks", "Turkish", "Portuguese", "Continental"
-    //             ],
-    //             "costForTwo": 30000,
-    //             "avgRating": "3.8",
-    //         }
-    //     },
-    //     {
-    //                 "type": "restaurant",
-    //                 "data": {
-    //                     "id": "65799",
-    //                     "name": "MCD",
-    //                     "deliveryTime": 20,
-    //                     "cloudinaryImageId": "xqwpuhgnsaf18te7zvtv",
-    //                     "cuisines": ["American", "Snacks", "Turkish", "Portuguese", "Continental"
-    //                     ],
-    //                     "costForTwo": 30000,
-    //                     "avgRating": "4.8",
-    //                 },
-        
-    //             },
-    //             {
-    //                 "type": "restaurant",
-    //                 "data": {
-    //                     "id": "65798",
-    //                     "name": "Dominos",
-    //                     "deliveryTime": 20,
-    //                     "cloudinaryImageId": "xqwpuhgnsaf18te7zvtv",
-    //                     "cuisines": [
-    //                         "American",
-    //                         "Snacks",
-    //                         "Turkish"
-    //                     ],
-    //                     "costForTwo": 30000,
-    //                     "avgRating": "4.3",
-    //                 },
-        
-    //             }
-        
-    // ]
-    )
+    const [listOfRestaurant, setListOfRestaurant] = useState([]);
+    const [filteredRestaurant,setFilteredRestaurant]=useState([])   
+     const [searchText,setSearchText]=useState("");
+    useEffect(() => {
+        fetchData();
+    }, []);
+    const fetchData = async () => {
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING ")
+        const json = await data.json();
+        console.log(json)
+        setListOfRestaurant(json?.data?.cards[2]?.data?.data?.cards);
+        setFilteredRestaurant(json?.data?.cards[2]?.data?.data?.cards);
 
-    return (
+    }
+
+    // if(listOfRestaurant.length===0){
+    //     return <Shimmer />
+    // }
+    return listOfRestaurant.length === 0 ? <Shimmer /> : (
         <div className='body'>
             <div className='filter'>
+            <div className="search">
+                <input type="text" className="search-btn" value={searchText} onChange={(e)=>{setSearchText(e.target.value)}}/>
+                <button onClick={()=>{
+                  const filteredRestaurant =  listOfRestaurant.filter((res)=>
+                  res.data.name.toLowerCase().includes(searchText.toLowerCase())
+                  );
+                  setFilteredRestaurant(filteredRestaurant);
+                }}>Search</button>
+            </div>
                 <button className="filter-btn"
                     onClick={() => {
                         const filteredList = listOfRestaurant.filter((res) =>
                             res.data.avgRating > 4
                         )
                         setListOfRestaurant(filteredList)
-                        console.log(listOfRestaurant)
+                        
                     }}
                 >Top Rated Restaurant</button>
             </div>
             <div className='res-container'>
                 {
-                    listOfRestaurant.map((restaurant) => (
+                    filteredRestaurant.map((restaurant) => (
 
                         <RestaurantCard key={restaurant.data.id} resData={restaurant} />
                     ))
